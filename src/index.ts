dad123f23a1dc80a6a13e027f59c428fff048e9e
Ly Nguyen <contact@minhlynguyenvo.com>
2024-01-24 13:46:09 +0100

    chore: export app

--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,9 @@ cron.schedule('* * * * * *', () => {
 });
 
 const app = express();
-const port = 5000;
 
 app.get('/', (request, response) => {
     response.json('OK');
 });
 
-app.listen(port, () => {
-    console.info(`App started on port ${port}.`);
-});
+export = app;
