d65feb5d14a76f8072adc3ae09f9dc600b017907
Ly Nguyen <contact@minhlynguyenvo.com>
2024-01-24 13:40:00 +0100

    chore: add express

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
-import cron from "node-cron";
+import cron from 'node-cron';
+import express from 'express';
 
 cron.schedule('* * * * * *', () => {
     console.log('running a task every second');
 });
 
-console.info("App started.");
+const app = express();
+const port = 5000;
+
+app.get('/', (request, response) => {
+    response.json('OK');
+});
+
+app.listen(port, () => {
+    console.info(`App started on port ${port}.`);
+});
--- src/stores.ts
+++ src/stores.ts
@@ -1,5 +1,4 @@
 import * as fs from 'node:fs';
-import * as path from 'node:path';
 
 export function readKey(key: string) {
     let content;
