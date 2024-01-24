e2faef85c2bd047f9810acada5ee2333d7cb838d
Ly Nguyen <contact@minhlynguyenvo.com>
2024-01-24 16:55:24 +0100

    chore: update

--- dist/index.js
+++ dist/index.js
@@ -2,14 +2,10 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
+Object.defineProperty(exports, "__esModule", { value: true });
 const node_cron_1 = __importDefault(require("node-cron"));
-const express_1 = __importDefault(require("express"));
 node_cron_1.default.schedule('* * * * * *', () => {
     console.log('running a task every second');
 });
-const app = (0, express_1.default)();
-app.get('/api', (request, response) => {
-    response.json('OK');
-});
-module.exports = app;
+console.info("App started.");
 //# sourceMappingURL=index.js.map
